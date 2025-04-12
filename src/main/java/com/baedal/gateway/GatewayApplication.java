4e0ba504726ca91d72ccf933735962b01bd55b31
handsone-u <handsoneu@gmail.com>
2025-04-12 17:01:31 +0900

    correct: 패키지 오타 수정

--- src/main/java/com/baedal/gateway/GatewayApplication.java
+++ src/main/java/com/baedal/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.bidmall.gateway;
+package com.baedal.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/baedal/gateway/GatewayApplicationTests.java
+++ src/test/java/com/baedal/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bidmall.gateway;
+package com.baedal.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
