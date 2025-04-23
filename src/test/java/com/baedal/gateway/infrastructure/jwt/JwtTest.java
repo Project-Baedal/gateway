d4b9c1d1dc4abed06d2deae22e380763af327aef
handsone-u <handsoneu@gmail.com>
2025-04-23 12:20:35 +0900

    [#2] test: 사용자 role 검증은 다운 스트림 서비스에서
    - 65ee41c98d0f90f8267efcea8610700f76b94835 에서 수정한 내용 테스트에 반영

--- src/test/java/com/baedal/gateway/infrastructure/jwt/JwtTest.java
+++ src/test/java/com/baedal/gateway/infrastructure/jwt/JwtTest.java
@@ -1,12 +1,12 @@
 package com.baedal.gateway.infrastructure.jwt;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import com.baedal.gateway.domain.model.Role;
 import io.jsonwebtoken.JwtException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JwtTest {
 
@@ -23,23 +23,15 @@ class JwtTest {
   void create_and_validate_token_success() {
     String token = jwtCreator.createToken(1L, Role.CUSTOMER.getRole());
 
-    assertThatCode(() -> jwtValidator.validateToken(token, Role.CUSTOMER.getRole()))
+    assertThatCode(() -> jwtValidator.validateToken(token))
         .doesNotThrowAnyException();
   }
-
-  @Test
-  void validate_token_fail_UNAUTHORIEZED_WRONG_ROLE() {
-    String token = jwtCreator.createToken(1L, Role.CUSTOMER.getRole());
-
-    assertThatThrownBy(() -> jwtValidator.validateToken(token, Role.RIDER.getRole()))
-        .isInstanceOf(RuntimeException.class);
-  }
-
+  
   @Test
   void validate_token_fail_TOKEN_STRANGE() {
     String token = "asd.adfadfa.eee";
 
-    assertThatThrownBy(() -> jwtValidator.validateToken(token, Role.CUSTOMER.getRole()))
+    assertThatThrownBy(() -> jwtValidator.validateToken(token))
         .isInstanceOf(JwtException.class);
   }
 }
